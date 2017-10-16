0b133c3f859e40d15b7201edda663054b417605c
Lex <oleksandr.pavliuk1308@gmail.com>
2017-10-16 21:22:12 +0300

    Розгалудження

---
+++ l6/main.js
@@ -0,0 +1,114 @@
+var a = 3;
+var b = 4;
+console.log("a =",a);
+console.log("b =",b);
+
+var plus = b + 3*a;
+console.log("plus =",plus);
+
+var minus = a - b;
+console.log("minus =",minus);
+
+var multiple = a * b;
+console.log("multiple =",multiple);
+
+var sub = a / b;
+console.log("sub =", sub);
+
+var o = a % b;
+console.log("sub =", o);
+
+var c2 = a*a + b*b;
+
+console.log("Квадрат гіпотенузи рівний ", c);
+
+var userName = " \"Саша\" ";
+console.log("userName =", userName);
+
+var userFriendName = ' \'Микола\' ';
+console.log("userFriendName =", userFriendName);
+var deliver = " ";
+var sum = userName + deliver + userFriendName + a + b;
+var numberString = "3";
+console.log("sum =", sum);
+
+var sum2 = 8;
+
+/*sum2 = sum2 + 2;*/
+// Coment
+sum2 += 2;
+console.log("sum2 =", sum2);
+
+var minus2 = 7;
+// minus2 = minus2 - 3;
+minus2 -= 3;
+console.log("minus2 =", minus2);
+
+var multiple2 = 7;
+// multiple2 = multiple2 * 3;
+multiple2 *= 3;
+console.log("multiple2 =", multiple2);
+
+
+var o2 = 15;
+// o2 = o2 / 3;
+o2 /= 3;
+console.log("o2 =", o2);
+
+var Name = "Petro", 
+name = "Kolia" , 
+nAme = 5;
+
+var empty;
+
+console.log("empty =", empty);
+
+var valueNall = null;
+
+console.log("valueNall =", valueNall);
+
+var i = 1;
+
+i = i - 1;
+i -= 1;
+i--;
+var c = i--;
+console.log("i =", i);
+console.log("c =", c);
+console.log("Катет а = "+ a +", катет b= " + b +".  Cума квадратів катетів ", c2);
+
+var valueTrue = true;
+var valueFalse = false;
+
+console.log("valueTrue = ", valueTrue);
+console.log("valueFalse = ", valueFalse);
+
+var valueBool = -8>=-5;
+
+console.log("valueBool = ", valueBool);
+
+var age = 99;
+var isAdmin = false;
+var adminName = "Петро";
+
+if(adminName == "Ivan"){
+	if(age>=18){
+		console.log("Ви можете керувати адмінкою");
+	}else{
+		console.log("Ви ще не повнолітній");
+	}
+}else{
+	console.log("Ви не адмін");
+}
+
+
+
+
+if("aa"<"aaaa"){
+	console.log("Відповідь позитивна");
+}else{
+	console.log("Відповідь негативна");
+}
+
+//var class return let const
+
