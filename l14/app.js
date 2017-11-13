9575f7d25c079f37325f49f36ca4eb7a35e1a63c
unknown <oleksandr.pavliuk1308@gmail.com>
2017-11-13 21:23:06 +0200

    L14 end

--- l14/app.js
+++ l14/app.js
@@ -62,18 +62,119 @@ let string = JSON.stringify(db);
 
 let stringObj = '{"name": "Sasha","pass":"1234"}';
 
-try{
+// try{
 
-	let obj = JSON.parse(stringObj);
+// 	let obj = JSON.parse(stringObj);
 
-} catch (error){
-	console.log(error);
-}
+// } catch (error){
+// 	console.log(error);
+// }
 
 //console.log(db);
-console.log(string);
+// console.log(string);
 //console.log(obj);
 
-fs.writeFile("db.txt", string, ()=>{
-	console.log('Файл мав записатись');
+// fs.writeFile("db.txt", string, (error) => {
+
+// 	if(error){
+// 		console.log('Неможливо записати файл');
+// 	}
+
+// 	console.log('Файл мав записатись');
+// });
+
+
+
+
+
+const stdin = process.stdin;
+const stdout = process.stdout;
+
+console.log("Якщо ви хочете додати користувача введіть 1");
+console.log("Якщо ви хочете зайти введіть 2");
+
+stdin.on("data", d => {
+	let convertD = parseInt(d.toString());
+	switch(convertD){
+		case 1:
+			addUser(d);
+			break;
+		case 2:
+			checkUser(d);
+			break;
+		default: 
+			console.log("Команда не коректна");
+	}
 });
+
+
+function addUser(d){
+	let arr = d.toString().split(" ");
+	let name = arr[1];
+	let pass = parseInt(arr[2]);
+
+	fs.readFile('db.txt', 'utf-8', (error, data) => {
+		if(error){
+			return console.log('Прочитати файл');
+		}
+
+		let rez = null;
+		try{
+			rez = JSON.parse(data);
+		}catch(e){
+			console.log("Неможливо роспарсити стрічку");
+		}
+
+		rez.push({name, pass});
+		console.log(rez);
+
+		let stringRez = JSON.stringify(rez);
+		fs.writeFile("db.txt", stringRez, (error) => {
+
+			if(error){
+				console.log('Неможливо записати файл');
+			}
+
+			console.log('Файл мав записатись');
+		});
+	});
+
+ 
+
+	console.log(name, pass);
+}
+
+function checkUser(d){
+	let arr = d.toString().split(" ");
+	let name = arr[1];
+	let pass = parseInt(arr[2]);
+
+	fs.readFile('db.txt', 'utf-8', (error, data) => {
+		if(error){
+			return console.log('Прочитати файл');
+		}
+
+		let rez = null;
+		try{
+			rez = JSON.parse(data);
+		}catch(e){
+			console.log("Неможливо роспарсити стрічку");
+		}
+
+		//let users = rez.find(item => true);
+		let haveUser = false;
+
+		for(let i = 0; i < rez.length; i++){
+			if(rez[i].name == name && rez[i].pass == pass ){
+				haveUser = true;
+				break;
+			}
+		}
+		
+		if(haveUser){
+			console.log("Ласкаво просимо на сайті");
+		} else {
+			console.log("Користувача немає");
+		}
+	});
+}
