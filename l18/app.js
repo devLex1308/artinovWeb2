df44e1886ac651a8d3e450d08f9ea1f12e3be5c0
unknown <oleksandr.pavliuk1308@gmail.com>
2017-11-27 19:29:21 +0200

    Add database

---
+++ l18/app.js
@@ -0,0 +1,197 @@
+let express = require('express');
+let path = require('path');
+let app = express();
+let bodyParser = require('body-parser');
+let fs = require('fs');
+var mongoClient = require("mongodb").MongoClient;
+var objectId = require("mongodb").ObjectID;
+var url = "mongodb://localhost:27017/test";
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('views', __dirname + '/template');
+app.set('view engine', 'ejs');
+ 
+app.use((req, res, next)=>{
+	//console.log(req.url);
+	//console.log('Test function 1');
+	next();
+});
+
+app.use('/',express.static(path.join(__dirname, 'public')));
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// });
+
+app.get('/about', function (req, res) {
+   res.render('about', {
+   	title: 'about',
+   	content: 'about content',
+   });
+});
+
+app.get('/info', function (req, res) {
+  res.render('info');
+});
+
+app.get('/help', function (req, res) {
+  res.render('page',{
+  	title: 'help',
+  	content: 'Content help'
+  });
+});
+
+app.get('/contact', function (req, res) {
+  res.render('page',{
+  	title: 'contact',
+  	content: 'Content contact'
+  });
+});
+
+app.get('/user/add', function (req, res) {
+  res.render('adduser',{
+  	title: 'Add user'
+  });
+});
+
+app.get('/user/edit/:id', function (req, res) {
+
+  load((error, data)=>{
+ 		if(error) {
+ 			res.send('Error 500 server not work!');
+ 		}
+ 		
+ 		let userId = req.params.id;
+ 		let user = data.data.find((item)=>{
+ 			return item.id == userId
+ 		});
+
+ 		res.render('edituser',{
+	  		title: 'All user',
+	  		user,
+	  	});
+
+ 	});
+});
+
+app.post('/user/edit/:id', function (req, res) {
+  console.log(req.body);
+  const {login, pass} = req.body;
+ 	
+ 	load((error, data)=>{
+ 		if(error) {
+ 			res.send('Error 500 server not work!');
+ 		}
+
+ 		let userId = req.params.id;
+ 		let userIndex = null;
+ 		let user = data.data.find((item, index) => {
+ 			if(item.id == userId){
+ 				userIndex = index;
+ 			}
+ 			return item.id == userId
+ 		});
+
+ 		data.data[userIndex] = {
+ 			...user,
+ 			login,
+ 			pass,
+ 		} 
+ 		console.log(userIndex, user);
+
+ 		save(data, (error)=>{
+ 			if(error){
+ 				res.send('Error 500 server not work!');
+ 			}
+
+ 			res.redirect('/users');
+ 		});
+ 	});
+
+});
+
+
+app.get('/users', function (req, res) {
+  
+  mongoClient.connect(url, function(err, db){
+        db.collection("users").find({}).toArray(function(err, users){
+            res.render('allUser',{
+			  	title: 'All user',
+			  	data: users,
+			 });
+            db.close();
+        });
+    });
+
+});
+
+app.post('/user/add', function (req, res) {
+
+  
+
+  if(!req.body) return res.sendStatus(400);
+     
+    const {login, pass} = req.body;
+    var user = {login, pass};
+     
+    mongoClient.connect(url, function(err, db){
+        db.collection("users").insertOne(user, function(err, result){
+             
+            if(err) return res.status(400).send();
+
+            db.close();
+            res.redirect('/users');
+        });
+    });
+
+});
+
+
+app.use((req, res, next)=>{
+	console.log(req.url);
+	res.send('Error 404 not found!');
+	next();
+});
+
+app.use((error, req, res, next)=>{
+	console.log(error);
+	console.log(req.url);
+	res.send('Error 500 server not work!');
+	next();
+});
+
+
+app.listen(3001);
+
+
+function load(done){
+	
+	fs.readFile('db.txt', 'utf-8', (error, data) => {
+		if(error){
+			done('Неможливо прочитати файл');
+		}
+
+		let rez = null;
+		try{
+			rez = JSON.parse(data);
+			done(null, rez);
+		}catch(e){
+			done('Неможливо розпарсити стрічку');
+		}
+
+	});
+}
+
+function save(data, done){
+	let stringRez = JSON.stringify(data);
+	fs.writeFile("db.txt", stringRez, (error) => {
+
+		if(error){
+			done('Неможливо записати файл');
+		}
+
+		done(null);
+	});
+}
---
+++ l18/public/js/custom.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('.share__text').customScroll();
+});
